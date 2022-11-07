19b7cbd4861c75e4eb5139e3602184332f71140b
Huiying Wang <whymissu1228@gmail.com>
2022-11-07 14:43:35 -0800

    added ResponseTemplateVO class

---
+++ src/main/java/com/dailycode/userservice/VO/ResponseTemplateVO.java
@@ -0,0 +1,16 @@
+package com.dailycode.userservice.VO;
+
+import com.dailycode.userservice.entity.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    
+    private User user;
+    private Department department;
+}
