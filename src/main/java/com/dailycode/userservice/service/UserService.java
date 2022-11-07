fed0ef09bc6e8d0ed89842f5e1f08afaf9cfcdbc
Huiying Wang <whymissu1228@gmail.com>
2022-11-07 12:08:00 -0800

    added addUser in UserService

--- src/main/java/com/dailycode/userservice/service/UserService.java
+++ src/main/java/com/dailycode/userservice/service/UserService.java
@@ -3,11 +3,17 @@ package com.dailycode.userservice.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dailycode.userservice.entity.User;
 import com.dailycode.userservice.repository.UserRepository;
 
 @Service
 public class UserService {
     
     @Autowired
-    private UserRepository userRepository; 
+    private UserRepository userRepository;
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
 }
