3cd2024509f253af9e1413df15b80ab844768ef5
Huiying Wang <whymissu1228@gmail.com>
2022-11-07 12:06:45 -0800

    added addUser method in UserController

--- src/main/java/com/dailycode/userservice/controller/UserController.java
+++ src/main/java/com/dailycode/userservice/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.dailycode.userservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dailycode.userservice.entity.User;
 import com.dailycode.userservice.service.UserService;
 
 @RestController
@@ -12,4 +15,10 @@ public class UserController {
     
     @Autowired
     private UserService userService;
+
+    @PostMapping("/")
+    public User addUser(@RequestBody User user) {
+
+        return userService.addUser(user);
+    }
 }
