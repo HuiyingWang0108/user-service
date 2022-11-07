ea2b6ce55ac2682e1ba1521c71c1812e0d3e764f
Huiying Wang <whymissu1228@gmail.com>
2022-11-07 15:47:10 -0800

    added ResTemplate Bean in UserserviceApplication

--- src/main/java/com/dailycode/userservice/UserServiceApplication.java
+++ src/main/java/com/dailycode/userservice/UserServiceApplication.java
@@ -2,6 +2,8 @@ package com.dailycode.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -9,5 +11,11 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+	
+	@Bean
+	private RestTemplate restTemplate() {
+		
+		return new RestTemplate();
+	};
 
 }
