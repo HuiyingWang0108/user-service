11cc078540dff8f2c203764789e0a87abcddb58b
Huiying Wang <whymissu1228@gmail.com>
2022-11-07 19:13:22 -0800

    added get obj department by restTemplate

--- src/main/java/com/dailycode/userservice/service/UserService.java
+++ src/main/java/com/dailycode/userservice/service/UserService.java
@@ -4,16 +4,21 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.dailycode.userservice.VO.Department;
 import com.dailycode.userservice.VO.ResponseTemplateVO;
 import com.dailycode.userservice.entity.User;
 import com.dailycode.userservice.repository.UserRepository;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public User addUser(User user) {
         return userRepository.save(user);
@@ -26,14 +31,17 @@ public class UserService {
         // get User by findById(userId)
         Optional<User> optional = userRepository.findById(userId);
         optional.ifPresent(user -> {
-            responseTemplateVO.setUser(user);
-        });
 
-        // get Department by RestTemplate
+            responseTemplateVO.setUser(user);
+
+            // get Department by RestTemplate
+            Department department = restTemplate
+                    .getForObject("http://localhost:9001/v1/departments/"
+                            + user.getDepartmentId(), Department.class);
+            responseTemplateVO.setDepartment(department);
+        });
 
         return responseTemplateVO;
     }
-
-  
 
 }
